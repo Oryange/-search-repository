7d98596184906dd624451830db9320f4f7f74466
Oryange <oryangestrifezze@gmail.com>
2021-07-13 13:58:51 -0300

    Adiciona alteraçao do basename

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './App.css';
 import Dash from './components/dash';
 import Details from './components/details';
 import Favorites from './components/favorites';
-import Footer from './components/footer';
+import Footer from './components/';
 import './index.css';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter basename="/hubbusters">
+    <BrowserRouter basename="/search-repository">
     <Switch>
       <Route exact path="/" component={ Dash } />
       <Route path="/detalhes" component={ Details } />
